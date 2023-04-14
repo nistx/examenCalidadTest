4a425d750975ab40d8dcadd6ca311a789c1782f8
Nicole Salazar <nist.jhs@gmail.com>
2023-04-14 09:33:30 -0500

    Test IsValidAmount

--- FinanceApp.Test/Validator/TransaccionValidatorTest.cs
+++ FinanceApp.Test/Validator/TransaccionValidatorTest.cs
@@ -128,4 +128,94 @@ public class TransaccionValidatorTest
         var resultado = validator.isValidAmountBasedCategory(rcMock.Object, transaccion);
         Assert.IsFalse(resultado);
     }
+
+    [Test]
+    public void IsValidAmountCaso01()
+    {
+        var validator = new TransaccionValidator();
+
+        var cuentas = new List<Cuenta>
+        {
+            new Cuenta { Id = 1 , Nombre = "Usuario1", Monto = 1000.0m }
+        };
+        var categorias = new List<Categoria>
+        {
+            new Categoria { Id = 1 , Tipo = "INGRESO"},
+            new Categoria { Id = 2 , Tipo = "EGRESO"}
+        };
+        var rcMock = new Mock<DbEntities>(new DbContextOptions<DbEntities>());
+        rcMock.Setup(o => o.Cuentas).ReturnsDbSet(cuentas);
+        rcMock.Setup(o => o.Categorias).ReturnsDbSet(categorias);
+
+        var transaccion = new Transaccion
+        {
+            CuentaId = 1,
+            CategoryId = 2,
+            Monto = -500.0m
+        };
+
+        var resultado = validator.IsValidAmount(rcMock.Object, transaccion);
+
+        Assert.IsTrue(resultado);
+    }
+    
+    [Test]
+    public void IsValidAmountCaso02()
+    {
+        var validator = new TransaccionValidator();
+
+        var cuentas = new List<Cuenta>
+        {
+            new Cuenta { Id = 1 , Nombre = "Usuario1", Monto = 1000.0m }
+        };
+        var categorias = new List<Categoria>
+        {
+            new Categoria { Id = 1 , Tipo = "INGRESO"},
+            new Categoria { Id = 2 , Tipo = "EGRESO"}
+        };
+        var rcMock = new Mock<DbEntities>(new DbContextOptions<DbEntities>());
+        rcMock.Setup(o => o.Cuentas).ReturnsDbSet(cuentas);
+        rcMock.Setup(o => o.Categorias).ReturnsDbSet(categorias);
+
+        var transaccion = new Transaccion
+        {
+            CuentaId = 1,
+            CategoryId = 2,
+            Monto = -10000.0m
+        };
+
+        var resultado = validator.IsValidAmount(rcMock.Object, transaccion);
+
+        Assert.IsFalse(resultado);
+    }
+    
+    [Test]
+    public void IsValidAmountCaso03()
+    {
+        var validator = new TransaccionValidator();
+
+        var cuentas = new List<Cuenta>
+        {
+            new Cuenta { Id = 1 , Nombre = "Usuario1", Monto = 60.0m }
+        };
+        var categorias = new List<Categoria>
+        {
+            new Categoria { Id = 1 , Tipo = "INGRESO"},
+            new Categoria { Id = 2 , Tipo = "EGRESO"}
+        };
+        var rcMock = new Mock<DbEntities>(new DbContextOptions<DbEntities>());
+        rcMock.Setup(o => o.Cuentas).ReturnsDbSet(cuentas);
+        rcMock.Setup(o => o.Categorias).ReturnsDbSet(categorias);
+
+        var transaccion = new Transaccion
+        {
+            CuentaId = 1,
+            CategoryId = 2,
+            Monto = 10.0m
+        };
+
+        var resultado = validator.IsValidAmount(rcMock.Object, transaccion);
+
+        Assert.IsTrue(resultado);
+    }
 }
